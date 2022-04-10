8f4a8672783fafab53128f04977fd02bb288491f
ricardo <ricleo45@yahoo.com>
2022-04-10 14:07:26 -0600

    added transportation

--- main.py
+++ main.py
@@ -1,33 +1,75 @@
 # source lists
+import random
 destinations = ['California', 'Utah', 'Colorado', 'Florida', 'New York' ]
 restaurants = ['Popeyes', 'MC Donalds', 'Applebees', 'Texas Road House', 'Gas Station']
-mode_of_transportation = ['Space Ship', 'Plane', 'Train', 'Bike', 'Bus']
-entertainment = ['Movies', 'National Park', 'Theme Park', '']
+transportation = ['Space Ship', 'Plane', 'Train', 'Bike', 'Bus']
+entertainment = ['Movies', 'TV shows', 'Magazines', 'Nap taking', '']
+ran_value1 = random.randint(0, 4)
+ran_value2 = random.randint(0, 4)
+ran_value3 = random.randint(0, 4)
+user_name = input('Please enter your Name Here: ')
+message = "Alright " + user_name + " let's being."
+print(message)
+# random destination choices 
+while True:
+    dest = input(f'Do you want to got to {destinations[ran_value1]} ? Enter y/n ')
+    if dest == 'y':
+        dest = destinations[ran_value1]
+        print('Great choice!')
+        break
+    elif dest == 'n':
+        break
+while dest == 'n':
+        dest = input(f'how about {destinations[ran_value2]} ? Enter y/n ')
+        if dest == 'y':
+            dest = destinations[ran_value2]
+            print('Perfect!')
+            break
+        elif dest == 'n':
+            break
+while dest == 'n':
+        if dest == 'y':
+            dest = input(f'or {destinations[ran_value3]} ? Enter y/n ')
+            dest = destinations[ran_value3]
+            print('Nice choise!')
+            break
+        elif dest == 'n':
+            print(f'Looks like we are out of choices {user_name} please try again :(')
+            break
 
-# ask for user name
-user_name = input('Please Enter Your Name Here: ')
-
-# random destination choices
-ask_destination = input('Hello ' + user_name + ' would you like to choose your destination today? y/n ')
-if ask_destination == 'y':
-    print(f'Great, would you like to go to {user_name} ?')
-    for list in destinations:
-            print(list)
-elif ask_destination != 'y':
-    print(f'My apologies for the inconveniece {user_name} check back soon! :)')
-else:
-    print('')
-
-
-
-# random restaurants choises
-
-# mode of transportaion choices
-
+# mode of transportation choices
+while True:
+    trans = input(f'Do you want to use {transportation[ran_value1]} ? Enter y/n ')
+    if trans == 'y':
+        trans = transportation[ran_value1]
+        print('Great choice!')
+        break
+    elif trans == 'n':
+        break
+while trans == 'n':
+        trans = input(f'how about a {transportation[ran_value2]} ? Enter y/n ')
+        if trans == 'y':
+            trans = transportation[ran_value2]
+            print('Perfect!')
+            break
+        elif trans == 'n':
+            break
+while trans == 'n':
+        if trans == 'y':
+            trans = input(f'or a {destinations[ran_value3]} ? Enter y/n ')
+            trans = transportation[ran_value3]
+            print('Nice choise!')
+            break
+        elif trans == 'n':
+            print(f'Looks like we are out of choices {user_name} please try again :(')
+            break
 # entertainment random choices
 
 # ability to (re)select random destinations, restaurants, mode of transportation, and/or entertainment
 
 # ability to confirm trip completion
-
+'''
+user_is_done = input('if this is your ideal trip Enter y to confirm and n to end session and retry. Enter y/n ')
+if user_is_done == 'y':
+'''
 # sum all functions into one final conclusion (each function should do just one thing!)
